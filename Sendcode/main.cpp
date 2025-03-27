0c143fba7dd29b8c846b73fe94b12e9d78b213d1
SamyWarnants <114075972+SamyWarnants@users.noreply.github.com>
2025-03-27 13:46:27 +0100

    dumb mistake

--- Sendcode/main.cpp
+++ Sendcode/main.cpp
@@ -1,47 +1,132 @@
 #include <iostream>
 #include <nzmqt/nzmqt.hpp>
+#include <nzmqt/impl.hpp>
 #include <QCoreApplication>
+#include <QString>
 #include <QTimer>
+#include <QThread>
+#include <QDateTime>
 
-const QString PUBLISH_TOPIC = "example>Aardappel>";  // Topic for sending messages
-
-void sendPoop(nzmqt::ZMQSocket* pusher)
+/*void printMessages( const QList<QByteArray>& messages )
 {
-    QByteArray message = PUBLISH_TOPIC.toUtf8() + "Aardappel REBORN";
-    pusher->sendMessage(message);
-    std::cout << "Sent: " << message.toStdString() << std::endl;
-}
+    for( const QByteArray & message : messages )
+    {
+        std::cout << message.toStdString() << std::endl;
+    }
+}*/
 
-int main(int argc, char *argv[])
+/*nzmqt::ZMQSocket *pusher;
+void pingMessage( void )
 {
-    QCoreApplication app(argc, argv);
-    std::cout << "Starting Aardappel Sender..." << std::endl;
+    nzmqt::ZMQMessage message = nzmqt::ZMQMessage( QString("PING").toUtf8() );
+    pusher->sendMessage( message );
+    std::cout << "PING send !" << std::endl;
+}*/
 
+int main( int argc, char *argv[] )
+{
+    QCoreApplication a(argc, argv);
+    std::cout << "Prep!" << std::endl;
     try
     {
-        // Create ZMQ context and PUSH socket
-        nzmqt::ZMQContext* context = nzmqt::createDefaultContext(&app);
-        nzmqt::ZMQSocket* pusher = context->createSocket(nzmqt::ZMQSocket::TYP_PUSH, context);
+        nzmqt::ZMQContext *context = nzmqt::createDefaultContext( &a );
+        nzmqt::ZMQSocket *pusher = context->createSocket( nzmqt::ZMQSocket::TYP_PUSH, context );
+        nzmqt::ZMQSocket *subscriber = context->createSocket( nzmqt::ZMQSocket::TYP_SUB, context );
+        //QObject::connect( subscriber, &nzmqt::ZMQSocket::messageReceived, printMessages );
+        QObject::connect( subscriber, &nzmqt::ZMQSocket::messageReceived, []( const QList<QByteArray>& messages )
+                         {
+                             if( messages.size() < 1 )
+                             {
+                                 std::cout << "Received empty message !" << std::endl;
+                             }
+                             else if( messages.size() == 1 )
+                             {
+                                 std::cout << "Received (" << messages.constFirst().size() << ") : " << messages.constFirst().toStdString() << std::endl;
+                             }
+                             else
+                             {
+                                 std::cout << "Received " << messages.size() << " parts" << std::endl;
+                                 int part = 1;
+                                 for( const QByteArray & message : messages )
+                                 {
+                                     std::cout << "Part " << part << " (" << message.size() << ") : " << message.toStdString() << std::endl;
+                                     part++;
+                                 }
+                             }
+                         } );
 
-        // ✅ Connect to Benternet's PUSH endpoint
-        pusher->connectTo("tcp://benternet.pxl-ea-ict.be:24041");
+        //		QTimer * pinger = new QTimer( context );
+        //QObject::connect( pinger, &QTimer::timeout, pingMessage );
+        //		QObject::connect( pinger, &QTimer::timeout, [pusher](void)
+        //		{
+        //			//QDateTime::currentDateTime().toString( Qt::ISODate );
+        //			nzmqt::ZMQMessage message = nzmqt::ZMQMessage( QDateTime::currentDateTime().toString( Qt::ISODate ).toUtf8() );//QString("PING").toUtf8() );
+        //			//nzmqt::ZMQMessage message = nzmqt::ZMQMessage( QString("Discord!help").toUtf8() );
+        //			pusher->sendMessage( message );
+        //			std::cout << "PING send !" << std::endl;
+        //		} );
+        //		pinger->setInterval( 1000*2 );
 
-        // Timer to send "poop" every 10 seconds
-        QTimer* poopTimer = new QTimer(context);
-        QObject::connect(poopTimer, &QTimer::timeout, [pusher]() {
-            sendPoop(pusher);
+        QThread *thread = QThread::create([pusher]{
+            QTextStream s(stdin);
+            while( 1 )
+            {
+                QString input = s.readLine();
+                nzmqt::ZMQMessage message = nzmqt::ZMQMessage( input.toUtf8() );
+                pusher->sendMessage( message );
+                std::cout << "Message send !" << std::endl;
+            }
         });
 
-        poopTimer->setInterval(5000); // Send every 10 seconds
-        poopTimer->start();
 
-        // Start the ZMQ event loop
+        //		pusher->connectTo( "tcp://localhost:24041" );
+        //		subscriber->connectTo( "tcp://localhost:24042" );
+        //		pusher->connectTo( "tcp://84.193.238.142:24041" );
+        //		subscriber->connectTo( "tcp://84.193.238.142:24042" );
+        pusher->connectTo( "tcp://benternet.pxl-ea-ict.be:24041" );
+        subscriber->connectTo( "tcp://benternet.pxl-ea-ict.be:24042" );
+        //		pusher->connectTo( "tcp://benternet.backup.pxl-ea-ict.be:24041" );
+        //		subscriber->connectTo( "tcp://benternet.backup.pxl-ea-ict.be:24042" );
+        //		pusher->connectTo( "tcp://np.pxl-ea-ict.be:24041" );
+        //		subscriber->connectTo( "tcp://np.pxl-ea-ict.be:24042" );
+        //		pusher->connectTo( "tcp://192.168.1.8:24041" );
+        //		subscriber->connectTo( "tcp://192.168.1.8:24042" );
+
+
+        if( argc > 1 )
+        {
+            for( int i = 1 ; i < argc ; i++ )
+            {
+                subscriber->subscribeTo( argv[i] );
+            }
+        }
+        else
+        {
+            subscriber->subscribeTo( "" );
+        }
+
+        if( !pusher->isConnected() || !subscriber->isConnected() )
+        {
+            std::cerr << "NOT CONNECTED !!!" << std::endl;
+        }
+
         context->start();
-    }
-    catch (nzmqt::ZMQException& ex)
-    {
-        std::cerr << "Caught an exception: " << ex.what() << std::endl;
-    }
+        //pinger->start();
+        thread->start();
 
-    return app.exec();
+        /*nzmqt::ZMQMessage * incoming = new nzmqt::ZMQMessage();
+        while( subscriber->isConnected() )
+        {
+            subscriber->receiveMessage( incoming, 0 );
+            std::cout << incoming->toByteArray().toStdString() << std::endl;
+        }*/
+    }
+    catch( nzmqt::ZMQException & ex )
+    {
+        std::cerr << "Caught an exception : " << ex.what();
+    }
+    std::cout << "Start!" << std::endl;
+    return a.exec();
 }
+message.txt
+6 KB
